da0ccf1d44a9104ac2d8cb59d9df47e08f25b4d4
imranruman <imran.hossain.ruman78@gmail.com>
2024-12-16 16:56:29 +0100

    timing added done

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,12 @@
 
 //new way of conditional function//
-const isVerified = ""
-console.log (` ${isVerified === true ? "user is verified" : "user is not verified"  } `);
+// const isVerified = ""
+// console.log (` ${isVerified === true ? "user is verified" : "user is not verified"  } `);
+
+// function getTimeString(time){
+//     const hour = parseInt(time/3600);
+//     let remainingSecond = time % 3600;
+//     const minute = parseInt(remainingSecond / 60);
+//     remainingSecond = remainingSecond % 60;
+//    return `${hour}hour ${minute}minute ${remainingSecond}second ago`;
+// }
--- scripts/videos.js
+++ scripts/videos.js
@@ -1,4 +1,14 @@
 console.log("video script added");
+// get hour and rest second
+
+function getTimeString(time){
+    const hour = parseInt(time/3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+   return `${hour}hour ${minute}minute ${remainingSecond}second ago`;
+}
+
 // ftech, show and load catagories in html//
 
 //create load catagories
@@ -29,7 +39,10 @@ card.innerHTML =
     <img
       src=${video.thumbnail}
       alt="Shoes" />
-      <span class = "absolute right-2 bottom-2 text-white bg-black rounded p-1">  ${video.others.posted_date} </span>
+      ${video.others.posted_date?.length == 0? "" :
+`        <span class = "absolute text-xs right-2 bottom-2 text-white bg-black rounded p-1">${getTimeString(video.others.posted_date)}</span>`
+      }
+
   </figure>
   <div class="px-0 py-2 flex gap-2">
     <div>
